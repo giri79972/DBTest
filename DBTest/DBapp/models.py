af04332abd890ca09610866c80a3ec16be5a17e4
giri79972 <48755929+giri79972@users.noreply.github.com>
2019-05-24 15:06:27 +0530

    Add files via upload

---
+++ DBTest/DBapp/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+class EMP(models.Model):
+    sno=models.IntegerField()
+    sname=models.CharField(max_length=20)
+    sal=models.IntegerField()
+
+    def __str__(self): #String Represention
+        return self.sname
